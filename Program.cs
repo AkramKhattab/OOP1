de1aef37348c3e6f50b555e76ce1475130d96ab1
Akram Khattab <Akram Khattab@AKRAM>
2024-07-11 21:32:35 +0300

    Part 1 - Question 2

--- Program.cs
+++ Program.cs
@@ -6,12 +6,17 @@ namespace C42_G04_OOP1
     {
         static void Main(string[] args)
         {
-            // Call the method to calculate the distance between two points
-            Part01_CalculateDistance();
+            // Commented out the code for the first question
+            // Part01_CalculateDistance();
+
+            // Call the method to find the oldest person
+            Part01_FindOldestPerson();
         }
 
-        #region Part 01 - Question 1.Create a struct called "Point" to represent a 2D point with properties "X" and "Y". Write a C# program that takes two points as input from the user and calculates the distance between them.
+        #region Part 1 - Question 1 : Create a struct called "Point" to represent a 2D point with properties "X" and "Y". Write a C# program that takes two points as input from the user and calculates the distance between them.
 
+        /*
+        
         public struct Point
         {
             public double X { get; set; }
@@ -39,6 +44,46 @@ namespace C42_G04_OOP1
 
             var distance = point1.DistanceTo(point2);
             Console.WriteLine($"The distance between the points is: {distance}");
+        }
+        */
+
+        #endregion
+
+        #region Part 1 - Question 2: .Create a struct called "Person" with properties "Name" and "Age". Write a C# program that takes details of 3 persons as input from the user and displays the name and age of the oldest person.
+        public struct Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+        }
+
+        public static void Part01_FindOldestPerson()
+        {
+            var persons = new Person[3];
+
+            for (var i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Enter details for Person {i + 1} (Name Age): ");
+                var personInput = Console.ReadLine().Split();
+                persons[i] = new Person(personInput[0], int.Parse(personInput[1]));
+            }
+
+            var oldest = persons[0];
+
+            foreach (var person in persons)
+            {
+                if (person.Age > oldest.Age)
+                {
+                    oldest = person;
+                }
+            }
+
+            Console.WriteLine($"The oldest person is {oldest.Name} with age {oldest.Age}");
         }
 
         #endregion
