6221d310f2ce2a48d9495fa341a864e02272131c
Akram Khattab <Akram Khattab@AKRAM>
2024-07-11 22:58:15 +0300

    Indexer

--- Program.cs
+++ Program.cs
@@ -84,3 +84,30 @@ class Program
 
 
 #endregion
+
+#region Indexer
+// Indexers allow instances of a class or struct to be indexed just like arrays.
+
+public class SampleCollection<T>
+{
+    private T[] arr = new T[100];
+
+    public T this[int i]
+    {
+        get { return arr[i]; }
+        set { arr[i] = value; }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var stringCollection = new SampleCollection<string>();
+        stringCollection[0] = "Hello";
+        Console.WriteLine(stringCollection[0]);  // Output: Hello
+    }
+}
+
+
+#endregion
